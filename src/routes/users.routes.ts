a94c6c28c81f2f44794cd80d61e4a08fe6ae1cd1
Joao Albuquerque <joaohcca@gmail.com>
2020-11-21 14:41:19 -0300

    adicionando mecanicas do avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,13 @@
-import { Router } from 'express';
+import { request, response, Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const UserRouter = Router();
-
+const upload = multer(uploadConfig)
 
 UserRouter.post('/', async (request, response) => {
   try {
@@ -31,4 +35,27 @@ UserRouter.post('/', async (request, response) => {
   }
 });
 
+UserRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+  try {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+   const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    })
+
+    const noPassUser = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    }
+    return response.json(noPassUser)
+  }  catch (err) {
+    return response.status(400).json({ error: err.message });
+  }  
+
+})
 export default UserRouter;
