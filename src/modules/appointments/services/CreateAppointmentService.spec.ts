d393cba96337a635739dc745912c010e120b06c2
joaohcca <joaohccalbu@gmail.com>
2021-05-04 22:36:05 -0300

    setting jest enviroment and adding 1st test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,21 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
-});
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentRepository';
+import CreateAppointmentService from './CreateAppointmentService'
+describe('CreateAppointment', () => {
+  it('it should be able to create a new appointment', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(fakeAppointmentRepository,);
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    })
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+
+  });
+
+  // it('it should be able to create a new appointment', () => {
+  //   expect('1+1').toBe(3);
+  // });
+})
