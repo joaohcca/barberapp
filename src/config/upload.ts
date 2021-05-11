16e81464db058c1e63ed7ac362e84abf1176ff36
joaohcca <joaohccalbu@gmail.com>
2021-05-11 20:02:53 -0300

    add coverage report and test to updateuseravatar

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,20 +2,20 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
-const tmpFolder =  path.resolve(__dirname,'..','..','tmp');
-
-
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
-    directory: tmpFolder,
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+  directory: tmpFolder,
 
-    storage: multer.diskStorage({
-        destination: path.resolve(__dirname, '..', '..', 'tmp'),
-        filename(resquest, file, callback) {
-            const fileHash = crypto.randomBytes(10).toString('hex');
-            const fileName = `${fileHash}-${file.originalname}`;
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    filename(resquest, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const fileName = `${fileHash}-${file.originalname}`;
 
-            return callback(null, fileName);
-        }
-    }),
+      return callback(null, fileName);
+    }
+  }),
 }
--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -9,6 +9,7 @@ import AppError from '@shared/errors/appError'
 import uploadConfig from '@config/upload';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 interface IRequest {
   user_id: string,
@@ -18,9 +19,12 @@ interface IRequest {
 @injectable()
 class UpdateUserAvatarService {
 
-  constructor(@inject('UsersRepository')
-  private usersRepository: IUsersRepository
-    ,) { }
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
+  ) { }
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
     const user = await this.usersRepository.findById(user_id)
@@ -30,19 +34,15 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar)
-
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath)
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
-    user.avatar = avatarFilename
+
+    const filename = await this.storageProvider.saveFile(avatarFilename)
+    user.avatar = filename;
 
     await this.usersRepository.save(user)
 
-    return user
+    return user;
 
   }
 
